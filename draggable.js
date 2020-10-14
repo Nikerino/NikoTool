626639d6181aa298f385bdc3a0b9644c043d873f
Niko Nikolopoulos <nnikolopoulos491@gmail.com>
2020-10-14 01:04:23 -0400

    Added full functionality for adding and removing lists and list items

--- draggable.js
+++ draggable.js
@@ -1,18 +1,6 @@
 const draggables = document.querySelectorAll( ".draggable" );
 const container = document.querySelector( ".draggable-container" );
 
-draggables.forEach( draggable => {
-
-	draggable.addEventListener( "dragstart", () => {
-		draggable.classList.add( "dragging" );
-	} )
-
-	draggable.addEventListener( "dragend", () => {
-		draggable.classList.remove( "dragging" );
-	} )
-
-} )
-
 container.addEventListener( "dragover", e => {
 
 	e.preventDefault();
